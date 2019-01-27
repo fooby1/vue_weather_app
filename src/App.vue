2df0946e0087ed26f3c5455b81d28aa87afdefe1
Joel Stephenson <joel.stephenson@me.com>
2019-01-27 23:51:52 +0000

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopBar msg="Weather App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TopBar
   }
 }
 </script>
